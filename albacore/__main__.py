a5d5635e7e96b93dd9e65f726da60c22355ee1ba
Tom Harrop <twharrop@gmail.com>
2017-08-22 11:58:16 +1200

    automatically activate env print before and after svg to logdir

--- albacore/__main__.py
+++ albacore/__main__.py
@@ -2,9 +2,33 @@
 
 import argparse
 import datetime
+import io
+import os
 import shutil
-import snakemake
 import subprocess
+import sys
+
+# make sure virtualenv is active or try to activate it
+try:
+    import snakemake
+except ImportError:
+    print("Missing snakemake, trying to find virtualenv")
+    try:
+        venv_path = [x for (x, y, z) in os.walk('.')
+                     if 'activate_this.py' in z][0]
+        virtualenv_file = os.path.join(venv_path, 'activate_this.py')
+        print("Trying to activate virtualenv: %s"
+              % virtualenv_file)
+        global_namespace = {
+            "__file__": virtualenv_file,
+            "__name__": "__main__",
+        }
+        with open(virtualenv_file, 'rb') as file:
+            exec(compile(file.read(), virtualenv_file, 'exec'),
+                 global_namespace)
+        import snakemake
+    except:
+        raise ImportError("Couldn't activate virtualenv")
 
 
 # FUNCTIONS
@@ -14,20 +38,32 @@ def generate_message(message_text):
     print('[ %s ]: %s' % (now, message_text))
 
 
+def print_graph(snakefile, config, dag_file):
+    # store old stdout
+    stdout = sys.stdout
+    # call snakemake api and capture output
+    sys.stdout = io.StringIO()
+    snakemake.snakemake(
+        snakefile,
+        config=config,
+        dryrun=True,
+        printdag=True)
+    output = sys.stdout.getvalue()
+    # restore sys.stdout
+    sys.stdout = stdout
+    # pipe the output to dot
+    with open(dag_file, 'wb') as svg:
+        dot_process = subprocess.Popen(
+            ['dot', '-Tsvg'],
+            stdin=subprocess.PIPE,
+            stdout=svg)
+        dot_process.communicate(input=output.encode())
+
+
 # GLOBALS
 snakefile = 'config/Snakefile'
 
-# check if virtualenv is active
-while not shutil.which("read_fast5_basecaller.py"):
-    virtualenv_file = "albacore_env/bin/activate_this.py"
-    generate_message("Trying to activate virtualenv\n%s" % virtualenv_file)
-    global_namespace = {
-        "__file__": virtualenv_file,
-        "__name__": "__main__",
-    }
-    with open(virtualenv_file, 'rb') as file:
-        exec(compile(file.read(), virtualenv_file, 'exec'), global_namespace)
-
+# did the virtualenv work?
 generate_message("Using virtualenv python3: %s" %
                  shutil.which("python3"))
 
@@ -35,37 +71,38 @@ generate_message("Using virtualenv python3: %s" %
 generate_message("Parsing files")
 parser = argparse.ArgumentParser()
 parser.add_argument(
-            '-y',
+            '--input',
             required=True,
-            help='other_input',
+            help='.tar.gz files containing raw Nanopore data folders',
             type=str,
-            dest='other_input',
+            dest='raw_data',
             action='append')
 parser.add_argument(
-            '-z',
+            '--outdir',
             required=True,
-            help='other_output',
+            help='Output directory',
             type=str,
-            dest='other_output')
+            dest='outdir')
 args = vars(parser.parse_args())
-other_input = [x for x in args['other_input']]
-other_output = args['other_output']
-print("Input file(s): %s" % other_input)
-print("Output folder: %s" % other_output)
+raw_data = [x for x in args['raw_data']]
+outdir = args['outdir']
+print("Input file(s): %s" % raw_data)
+print("Output folder: %s" % outdir)
+
+# set up logging
+log_dir = os.path.join(outdir, 'logs')
+args['log_dir'] = log_dir
+if not os.path.isdir(log_dir):
+    os.makedirs(log_dir)
+
+# print before dag
+print_graph(snakefile, args, os.path.join(log_dir, "before.svg"))
 
 #run the pipeline
 print("args: %s" % args)
 snakemake.snakemake(
     snakefile=snakefile,
-    config=args,
-    printdag=True)
+    config=args)
 
-# print the graph CURRENTLY NOT WORKING, NEED TO USE API TO GET DAG
-# dot_out = open('config/dag.svg', 'wb', 0)
-# dot_in = subprocess.Popen(
-#     ['dot', '-Tsvg'],
-#     stdin=subprocess.PIPE, stdout=dot_out).stdin
-# dag_proc = subprocess.Popen(
-#     ['snakemake', '--snakefile', snakefile, '--dag', ],
-#     stdout=dot_in)
-# dag_proc.communicate()
+# print after dag
+print_graph(snakefile, args, os.path.join(log_dir, "after.svg"))
