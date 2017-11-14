ef4f4bcd956b9e32d28c101d3ecead1432fb7722
Tom Harrop <twharrop@gmail.com>
2017-11-14 14:09:07 +1300

    need to target plot

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def readme():
 # main setup script
 setup(
     name='basecall_wrapper',
-    version='0.0.17',
+    version='0.0.18',
     description='Tom\'s wrapper for ONT albacore',
     long_description=readme(),
     url='https://github.com/TomHarrop/basecall_wrapper',
