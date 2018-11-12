7a6e4614195109442559a49d0d0f502b9ce42ef1
Tom Harrop <twharrop@gmail.com>
2018-11-13 11:48:12 +1300

    release 0.0.32

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def readme():
 # main setup script
 setup(
     name='basecall_wrapper',
-    version='0.0.31',
+    version='0.0.32',
     description='Tom\'s wrapper for ONT albacore',
     long_description=readme(),
     url='https://github.com/TomHarrop/basecall_wrapper',
